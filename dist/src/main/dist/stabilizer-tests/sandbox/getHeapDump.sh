b43684b0c6f166b9680d9f4660b4c5da65c8ce36
danny <danny@hazelcast.com>
2014-10-22 17:31:29 +0300

    heap hog

--- dist/src/main/dist/stabilizer-tests/sandbox/getHeapDump.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/getHeapDump.sh
@@ -13,11 +13,13 @@ for (( i=1; i<=$max; i++ ))
 do
     for box in $ips
     do
-        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "rm -f heap.bin; jps | grep Member.* | cut -d ' ' -f1 | xargs -L 1  jmap -histo $1 > histo.txt"
-
-        scp stabilizer@${box}: ${box}MemberHeap.bin
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "rm -f heap.bin; jps | grep Member.* | cut -d ' ' -f1 | xargs -L 1  jmap -dump:file=heap.bin $1"
     done
 
+    for box in $ips
+    do
+        scp stabilizer@${box}: ${box}MemberHeap${i}.bin
+    done
 
     sleep ${everySeconds}
 done
