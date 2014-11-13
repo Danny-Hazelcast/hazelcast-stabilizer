7d8aa2cb8019db37b7514eb50cdddacb2d0f34c8
danny <danny@hazelcast.com>
2014-11-13 14:04:36 +0200

    ee tests

---
+++ eeTests/src/main/java/com/hazelcast/stabilizer/tests/ee/EeTests.java
@@ -0,0 +1,165 @@
+package com.hazelcast.stabilizer.tests.ee;
+
+
+import com.hazelcast.core.IMap;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.stabilizer.probes.probes.IntervalProbe;
+import com.hazelcast.stabilizer.probes.probes.SimpleProbe;
+import com.hazelcast.stabilizer.tests.TestContext;
+import com.hazelcast.stabilizer.tests.annotations.Performance;
+import com.hazelcast.stabilizer.tests.annotations.Run;
+import com.hazelcast.stabilizer.tests.annotations.Setup;
+import com.hazelcast.stabilizer.tests.annotations.Teardown;
+import com.hazelcast.stabilizer.tests.annotations.Warmup;
+import com.hazelcast.stabilizer.tests.map.helpers.KeyUtils;
+import com.hazelcast.stabilizer.tests.map.helpers.StringUtils;
+import com.hazelcast.stabilizer.tests.utils.KeyLocality;
+import com.hazelcast.stabilizer.tests.utils.TestUtils;
+import com.hazelcast.stabilizer.tests.utils.ThreadSpawner;
+
+import com.hazelcast.nio.serialization.EnterpriseSerializationService;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class EeTests {
+
+    private final static ILogger log = Logger.getLogger(EeTests.class);
+
+    private EnterpriseSerializationService serializationService =null;
+    //props
+    public int writePercentage = 10;
+    public int threadCount = 10;
+    public int keyLength = 10;
+    public int valueLength = 10;
+    public int keyCount = 10000;
+    public int valueCount = 10000;
+    public int logFrequency = 10000;
+    public int performanceUpdateFrequency = 10000;
+    public boolean usePut = true;
+    public String basename = "";
+    public KeyLocality keyLocality = KeyLocality.Random;
+    public int minNumberOfMembers = 0;
+
+    //probes
+    public IntervalProbe getLatency;
+    public IntervalProbe putLatency;
+    public SimpleProbe throughput;
+
+    private IMap<String, String> map;
+    private String[] keys;
+    private String[] values;
+    private final AtomicLong operations = new AtomicLong();
+    private TestContext testContext;
+
+    private HazelcastInstance targetInstance;
+
+    @Setup
+    public void setup(TestContext testContext) throws Exception {
+        if (writePercentage < 0) {
+            throw new IllegalArgumentException("Write percentage can't be smaller than 0");
+        }
+
+        if (writePercentage > 100) {
+            throw new IllegalArgumentException("Write percentage can't be larger than 100");
+        }
+
+        this.testContext = testContext;
+        targetInstance = testContext.getTargetInstance();
+        map = targetInstance.getMap(basename + "-" + testContext.getTestId());
+    }
+
+    @Teardown
+    public void teardown() throws Exception {
+        map.destroy();
+        log.info(TestUtils.getOperationCountInformation(targetInstance));
+    }
+
+    @Warmup(global = false)
+    public void warmup() throws InterruptedException {
+        TestUtils.waitClusterSize(log, targetInstance, minNumberOfMembers);
+        keys = KeyUtils.generateKeys(keyCount, keyLength, keyLocality, testContext.getTargetInstance());
+        values = StringUtils.generateStrings(valueCount, valueLength);
+
+        Random random = new Random();
+        for (int k = 0; k < keys.length; k++) {
+            String key = keys[random.nextInt(keyCount)];
+            String value = values[random.nextInt(valueCount)];
+            map.put(key, value);
+        }
+    }
+
+    @Run
+    public void run() {
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+        for (int k = 0; k < threadCount; k++) {
+            spawner.spawn(new Worker());
+        }
+        spawner.awaitCompletion();
+    }
+
+    @Performance
+    public long getOperationCount() {
+        return operations.get();
+    }
+
+    private class Worker implements Runnable {
+        private final Random random = new Random();
+
+        @Override
+        public void run() {
+            long iteration = 0;
+            while (!testContext.isStopped()) {
+
+                String key = randomKey();
+
+                if (shouldWrite(iteration)) {
+                    String value = randomValue();
+                    putLatency.started();
+                    if (usePut) {
+                        map.put(key, value);
+                    } else {
+                        map.set(key, value);
+                    }
+                    putLatency.done();
+                } else {
+                    getLatency.started();
+                    map.get(key);
+                    getLatency.done();
+                }
+
+                if (iteration % logFrequency == 0) {
+                    log.info(Thread.currentThread().getName() + " At iteration: " + iteration);
+                }
+
+                if (iteration % performanceUpdateFrequency == 0) {
+                    operations.addAndGet(performanceUpdateFrequency);
+                }
+
+                iteration++;
+                throughput.done();
+            }
+        }
+
+        private String randomValue() {
+            return values[random.nextInt(values.length)];
+        }
+
+        private String randomKey() {
+            int length = keys.length;
+            return keys[random.nextInt(length)];
+        }
+
+        private boolean shouldWrite(long iteration) {
+            if (writePercentage == 0) {
+                return false;
+            } else if (writePercentage == 100) {
+                return true;
+            } else {
+                return (iteration % 100) < writePercentage;
+            }
+        }
+    }
+}
