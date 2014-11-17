b53476097f6dd5441dedd982158ae011cefe4bcf
danny <danny@hazelcast.com>
2014-11-18 00:29:58 +0200

    enterprise jcache tests

---
+++ eetest/src/main/java/com/stabilizer/enterprise/tests/icache/CachePutTest.java
@@ -0,0 +1,109 @@
+package com.stabilizer.enterprise.tests.icache;
+
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
+import com.hazelcast.config.CacheSimpleConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.core.Member;
+import com.hazelcast.core.Partition;
+import com.hazelcast.core.PartitionService;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.stabilizer.tests.TestContext;
+import com.hazelcast.stabilizer.tests.annotations.Run;
+import com.hazelcast.stabilizer.tests.annotations.Setup;
+import com.hazelcast.stabilizer.tests.annotations.Verify;
+import com.hazelcast.stabilizer.tests.utils.TestUtils;
+import com.hazelcast.stabilizer.tests.utils.ThreadSpawner;
+import org.HdrHistogram.IntCountsHistogram;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.spi.CachingProvider;
+import java.util.Random;
+
+public class CachePutTest {
+    private final static ILogger log = Logger.getLogger(CachePutTest.class);
+
+    public int threadCount = 3;
+    public int maxValueLength = 10000;
+    public int minValueLength = 1000;
+    public int durationSec = 0;
+    public String basename;
+
+    private TestContext testContext;
+    private HazelcastInstance targetInstance;
+
+    private Cache<Object, Object> cache;
+
+    @Setup
+    public void setup(TestContext testContex) throws Exception {
+        testContext = testContex;
+        targetInstance = testContext.getTargetInstance();
+        basename = basename+""+testContex.getTestId();
+
+        CachingProvider cachingProvider = HazelcastServerCachingProvider.createCachingProvider(targetInstance);
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        cache = cacheManager.getCache(basename);
+    }
+
+    @Run
+    public void run() {
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+
+        Worker[] workers = new Worker[threadCount];
+
+        for(int i=0; i<threadCount; i++){
+            workers[i] = new Worker();
+            spawner.spawn(workers[i]);
+        }
+        spawner.awaitCompletion();
+
+        for(int i=1; i<threadCount; i++){
+            workers[0].putLatencyHisto.add(workers[i].putLatencyHisto);
+        }
+        targetInstance.getList(basename+"putHisto").add(workers[0].putLatencyHisto);
+    }
+
+    private class Worker implements Runnable {
+        IntCountsHistogram putLatencyHisto = new IntCountsHistogram(1, 1000*30, 0);
+        Random random = new Random();
+
+        public void run() {
+            while(!testContext.isStopped()){
+                int key = random.nextInt();
+                int size = random.nextInt(maxValueLength-minValueLength) + maxValueLength;
+                byte[] value = new byte[size];
+
+                long start = System.currentTimeMillis();
+                cache.put(key, value);
+                long stop = System.currentTimeMillis();
+                putLatencyHisto.recordValue(stop - start);
+            }
+        }
+    }
+
+    @Verify(global = true)
+    public void verify() throws Exception {
+
+        CacheSimpleConfig cacheConfig = targetInstance.getConfig().getCacheConfig(basename);
+        log.info(basename+": "+cacheConfig);
+
+        IList<IntCountsHistogram> putHistos = targetInstance.getList(basename+"putHisto");
+
+        IntCountsHistogram putHisto = putHistos.get(0);
+
+        for(int i=1; i<putHistos.size(); i++){
+            putHisto.add(putHistos.get(i));
+        }
+
+        System.out.println(basename + ": Put Latency Histogram");
+        putHisto.outputPercentileDistribution(System.out, 1.0);
+        double putsPerSec = putHisto.getTotalCount() / (durationSec/1000);
+
+
+        log.info(basename+": puts ="+putHisto.getTotalCount());
+        log.info(basename+": avg put/sec ="+putsPerSec);
+    }
+}
---
+++ eetest/src/main/java/com/stabilizer/enterprise/tests/icache/CasICacheTest.java
@@ -0,0 +1,142 @@
+package com.stabilizer.enterprise.tests.icache;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;;
+import com.hazelcast.cache.impl.HazelcastServerCacheManager;
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
+import com.hazelcast.client.cache.impl.HazelcastClientCacheManager;
+import com.hazelcast.client.cache.impl.HazelcastClientCachingProvider;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.monitor.LocalMemoryStats;
+import com.hazelcast.stabilizer.tests.TestContext;
+import com.hazelcast.stabilizer.tests.TestRunner;
+import com.hazelcast.stabilizer.tests.annotations.Run;
+import com.hazelcast.stabilizer.tests.annotations.Setup;
+import com.hazelcast.stabilizer.tests.annotations.Verify;
+import com.hazelcast.stabilizer.tests.annotations.Warmup;
+import com.hazelcast.stabilizer.tests.utils.TestUtils;
+import com.hazelcast.stabilizer.tests.utils.ThreadSpawner;
+import com.stabilizer.enterprise.tests.icache.helpers.MemoryStatsUtil;
+
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This tests the cas method: replace. So for optimistic concurrency control.
+ * <p/>
+ * We have a bunch of predefined keys, and we are going to concurrently increment the value
+ * and we protect ourselves against lost updates using cas method replace.
+ * <p/>
+ * Locally we keep track of all increments, and if the sum of these local increments matches the
+ * global increment, we are done
+ */
+public class CasICacheTest {
+
+    private final static ILogger log = Logger.getLogger(CasICacheTest.class);
+
+    public int threadCount = 10;
+    public int keyCount = 1000;
+    public String basename;
+
+    private TestContext testContext;
+    private HazelcastInstance targetInstance;
+    private CacheManager cacheManager;
+    private Cache<Integer, Long> cache;
+    private LocalMemoryStats memoryStats;
+
+    @Setup
+    public void setup(TestContext testContext) throws Exception {
+        this.testContext = testContext;
+        targetInstance = testContext.getTargetInstance();
+        basename = basename+""+testContext.getTestId();
+
+        if (TestUtils.isMemberNode(targetInstance)) {
+            HazelcastServerCachingProvider hcp = new HazelcastServerCachingProvider();
+            cacheManager = new HazelcastServerCacheManager(
+                    hcp, targetInstance, hcp.getDefaultURI(), hcp.getDefaultClassLoader(), null);
+        } else {
+            HazelcastClientCachingProvider hcp = new HazelcastClientCachingProvider();
+            cacheManager = new HazelcastClientCacheManager(
+                    hcp, targetInstance, hcp.getDefaultURI(), hcp.getDefaultClassLoader(), null);
+        }
+        cache = cacheManager.getCache(basename);
+
+        if ( TestUtils.isMemberNode(targetInstance) ){
+            memoryStats = MemoryStatsUtil.getMemoryStats(targetInstance);
+
+            log.info(basename+": "+memoryStats);
+        }
+    }
+
+    @Warmup(global = true)
+    public void warmup() throws Exception {
+        for (int k = 0; k < keyCount; k++) {
+            cache.put(k, 0l);
+        }
+    }
+
+    @Run
+    public void run() {
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+        for (int k = 0; k < threadCount; k++) {
+            spawner.spawn(new Worker());
+        }
+        spawner.awaitCompletion();
+    }
+
+    private class Worker implements Runnable {
+        private final Random random = new Random();
+        private final long[] increments = new long[keyCount];
+
+        public void run() {
+            while (!testContext.isStopped()) {
+                int key = random.nextInt(keyCount);
+                long increment = random.nextInt(100);
+
+                Long current = cache.get(key);
+                if (cache.replace(key, current, current + increment)) {
+                    increments[key] += increment;
+                }
+
+            }
+            targetInstance.getList(basename).add(increments);
+        }
+    }
+
+    @Verify(global = true)
+    public void verify() throws Exception {
+        long[] amount = new long[keyCount];
+
+        IList<long[]> resultsPerWorker = targetInstance.getList(basename);
+        for (long[] incrments : resultsPerWorker) {
+            for (int i=0 ; i<keyCount; i++) {
+                amount[i] += incrments[i];
+            }
+        }
+
+        int failures = 0;
+        for (int k = 0; k < keyCount; k++) {
+            long expected = amount[k];
+            long found = cache.get(k);
+            if (expected != found) {
+                failures++;
+            }
+        }
+
+        if ( TestUtils.isMemberNode(targetInstance) ){
+            log.info(basename+": "+memoryStats.toString());
+        }
+
+        assertEquals(basename+" "+failures+" key=>values have been incremented unExpected", 0, failures);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        CasICacheTest test = new CasICacheTest();
+        new TestRunner(test).run();
+    }
+}
