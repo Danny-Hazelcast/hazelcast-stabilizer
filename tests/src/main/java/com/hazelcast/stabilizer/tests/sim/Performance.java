aa4d5edd447e81be259cb01c61939bc6f44b27b4
danny <danny@hazelcast.com>
2015-02-10 09:45:41 +0200

    aws onlt creating r3

--- tests/src/main/java/com/hazelcast/stabilizer/tests/sim/Performance.java
+++ tests/src/main/java/com/hazelcast/stabilizer/tests/sim/Performance.java
@@ -1,12 +1,16 @@
 package com.hazelcast.stabilizer.tests.sim;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.CsvReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
-import org.HdrHistogram.Histogram;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Random;
@@ -17,25 +21,28 @@ import java.util.concurrent.Executors;
 
 public class Performance {
 
-    private static final long MAXIMUM_LATENCY = 1000 * 10; // 10 sec
-    private static final int  SIG_DIGS = 4;
-    private static final double outputScalingRatio = 1.0;
+    public File cvsDir = new File( System.getProperty("cvsDir", "/Users/danny") );
 
-    public String clientHzFile = "client-hazelcast.xml";
-    public HazelcastInstance client;
+    public String clientHzFile = System.getProperty("clientHzFile", "client-hazelcast.xml");
+    public String baseMapName= System.getProperty("baseMapName", "m");
+    public int totalMaps =  Integer.parseInt(System.getProperty("totalMaps", "10"));
+    public int totalKeys = Integer.parseInt(System.getProperty("totalKeys", "10"));
+    public int valueByteArraySize = Integer.parseInt(System.getProperty("valueByteArraySize", "10"));
 
-    public String baseMapName="m";
-    public int totalMaps = 10;
-    public int totalKeys = 10;
-    public int threadCount = 4;
+    public double putProb=Double.parseDouble(System.getProperty("putProb", "0.5"));
+    public double getProb=Double.parseDouble(System.getProperty("getProb", "0.5"));
+    public double setProb=Double.parseDouble(System.getProperty("setProb", "0.0"));
 
+    public int threadCount = Integer.parseInt(System.getProperty("threadCount", "8"));;
+    public boolean sharedMetrics = Boolean.parseBoolean(System.getProperty("sharedMetrics", "true"));;
+
+    private HazelcastInstance client;
+    private ExecutorService executor;
+    private Set<MyTask> tasks;
     private int durationSeconds=0;
 
-    public ExecutorService executor;
-    public Set<MyTask> tasks;
 
     public Performance() throws IOException {
-
         XmlClientConfigBuilder configBuilder = new XmlClientConfigBuilder(clientHzFile);
         ClientConfig clientConfig = configBuilder.build();
         client = HazelcastClient.newHazelcastClient(clientConfig);
@@ -50,89 +57,96 @@ public class Performance {
 
     public void run(int seconds) throws InterruptedException {
         durationSeconds = seconds;
+        MetricRegistry metrics = new MetricRegistry();
+
         for(MyTask t: tasks) {
             t.setDuration(durationSeconds);
+
+            if(sharedMetrics){
+                t.metrics = metrics;
+            }else{
+                t.metrics = new MetricRegistry();
+            }
         }
         executor.invokeAll(tasks);
     }
 
     public void printResults(){
+        for(MyTask t : tasks){
 
-        Histogram histogram = new Histogram(MAXIMUM_LATENCY, SIG_DIGS);
-        for(MyTask t: tasks) {
-            histogram.add(t.histogram);
+            ConsoleReporter reporter = ConsoleReporter.forRegistry(t.metrics).build();
+            CsvReporter csv = CsvReporter.forRegistry(t.metrics).build(cvsDir);
 
+            csv.report();
+            reporter.report();
+
+            if(sharedMetrics){
+                break;
+            }
         }
-        histogram.outputPercentileDistribution(System.out, outputScalingRatio);
-
-
-        long totalops = histogram.getTotalCount();
-
-        long start = histogram.getStartTimeStamp();
-        long end = histogram.getEndTimeStamp();
-
-
-        System.out.println("total operations="+totalops);
-        System.out.println("duration sec ="+durationSeconds);
-        System.out.println("opps / sec  ="+totalops / durationSeconds);
-
-
-        /*
-        long val = histogram.getValueAtPercentile(1.0);
-        System.out.println("100% ="+val);
-
-        AbstractHistogram.AllValues all = histogram.allValues();
-        for( HistogramIterationValue v : all ){
-            System.out.println(v.toString());
-        }
-        */
     }
 
     private class MyTask implements Callable<Object> {
 
-        public final Histogram histogram = new Histogram(MAXIMUM_LATENCY, SIG_DIGS);
-        public Random random = new Random();
+        public MetricRegistry metrics;
+        private Timer getTimer;
+        private Timer putTimer;
+        private Timer setTimer;
 
+        private Random random = new Random();
         private int duration = 0;
+        private byte[] value = new byte[valueByteArraySize];
+
+        public MyTask(){
+            random.nextBytes(value);
+        }
 
         public void setDuration(int seconds){
             duration = seconds * 1000;
         }
 
         public Object call() throws Exception {
+            getTimer = metrics.timer("getTimer");
+            putTimer = metrics.timer("putTimer");
+            setTimer = metrics.timer("setTimer");
 
-            histogram.reset();
-
-            long startTime=System.currentTimeMillis();
-            histogram.setStartTimeStamp(startTime);
-
-            while( (System.currentTimeMillis()-startTime) < duration ){
-
+            long endTime = System.currentTimeMillis() + duration;
+            while( System.currentTimeMillis() < endTime ){
                 IMap map = client.getMap(baseMapName+random.nextInt(totalMaps));
 
-                long start = System.currentTimeMillis();
-                Object o = map.get(random.nextInt(totalKeys));
-                long end = System.currentTimeMillis();
+                double chance = random.nextDouble();
 
-                histogram.recordValue(end - start);
+                if((chance -= putProb)<=0) {
+                    Timer.Context context =  putTimer.time();
+                      Object o = map.put(random.nextInt(totalKeys), value);
+                      //Thread.sleep(random.nextInt(10));
+                    context.stop();
 
-                System.out.println("got = "+o);
+                }else if((chance-=getProb)<=0){
+                    Timer.Context context =  getTimer.time();
+                      Object o = map.get(random.nextInt(totalKeys));
+                      //Thread.sleep(random.nextInt(10));
+                    context.stop();
+
+                }else{
+                    Timer.Context context =  setTimer.time();
+                      map.set(random.nextInt(totalKeys), value);
+                      //Thread.sleep(random.nextInt(10));
+                    context.stop();
+                }
             }
-
-            histogram.setEndTimeStamp(System.currentTimeMillis());
-            System.out.println("end");
             return null;
         }
     }
 
     public static void main(String[] args) throws Throwable {
+        int jitWarmUpSec = Integer.parseInt(System.getProperty("jitWarmUpSec", "10"));
+        int durationSec = Integer.parseInt(System.getProperty("durationSec", "30"));
 
         Performance p = new Performance();
-        p.run(5);
-        p.printResults();
+        p.run(jitWarmUpSec);
 
-        p.run(5);
+        p.run(durationSec);
         p.printResults();
-
     }
 }
